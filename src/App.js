25062cf089fb5869da91c87fd9bd16ab4aff6aed
komalkala <komalkala42@gmail.com>
2024-02-02 11:05:11 +0530

    country card

--- src/App.js
+++ src/App.js
@@ -45,29 +45,29 @@ function App() {
 
   return (
     <>
-     <div className='headerStyle'>
-        <input
-          type="text"
-          placeholder="Search for countries..."
-          className='searchStyle'
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </div>
-      <div className='containerStyle'>
-        {filteredCountries.map((country) => (
-          <div key={country.cca3} style={cardStyle}>
-            <img
-              src={country.flags.png}
-              alt={`Flag of ${country.name.common}`}
-              style={imageStyle}
-            />
-            <p>{country.name.common}</p>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+    <div className='headerStyle'>
+       <input
+         type="text"
+         placeholder="Search for countries..."
+         className='searchStyle'
+         value={searchTerm}
+         onChange={(e) => setSearchTerm(e.target.value)}
+       />
+     </div>
+     <div className='containerStyle'>
+       {filteredCountries.map((country) => (
+         <div key={country.cca3} className='countryCard' style={cardStyle}>
+           <img
+             src={country.flags.png}
+             alt={`Flag of ${country.name.common}`}
+             style={imageStyle}
+           />
+           <p>{country.name.common}</p>
+         </div>
+       ))}
+     </div>
+   </>
+ );
 }
 
 export default App;
